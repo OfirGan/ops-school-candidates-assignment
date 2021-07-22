71eb5ee4cebc64a094736ae6e372d32324fd2aa3
Ofir Gan <offirgan@gmail.com>
2021-07-22 13:52:30 +0300

    Done

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo head -n -3 /etc/apache2/sites-available/000-default.conf > 000-default.temp 
+sudo mv 000-default.temp /etc/apache2/sites-available/000-default.conf
+rm -f 000-default.temp
+sudo systemctl restart apache2
